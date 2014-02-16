d8ab7537f055470e183f2a3be76b23003cb7b51a
Kubi <kubanka.peter@gmail.com>
2014-02-16 15:29:40 +0100

    some security options

--- server/web/source.php
+++ server/web/source.php
@@ -5,6 +5,16 @@ use DTable\Db\Handle;
 
 require_once __DIR__ . "/../Bootup.php";
 
+$referer = $_SERVER["HTTP_REFERER"];
+$url = parse_url($referer);
+
+if ($url['host'] !== 'dtable.devdrive.org')
+{
+    header('HTTP/1.0 403 Forbidden');
+    echo 'Forbidden';
+    die();
+}
+
 $handle = new Handle();
 
 if (isset($_GET['table']) && $_GET['table'] == 'example_02')
